b3b7990230b43be31546573dab519184c3809afe
balakrishnan <balakrishnanultra01@gmail.com>
2026-03-28 22:28:16 +0530

    integer fuzzing data generate the value and testing the most used function.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,8 @@
-//Integer data Generator function
-// signed
+//Integer Fuzzing data Generator function
+// signed. write the code in comming day
 fun byte_data_generator(){}
 fun short_data_generator(){}
+fun long_data_generator(){}
 fun int_data_generator(user_set_limit:Int):MutableList<Int>
 {
     var generator_value_list = mutableListOf<Int>()
@@ -12,46 +13,54 @@ fun int_data_generator(user_set_limit:Int):MutableList<Int>
         generator_value++
     }
     return generator_value_list
-
 }
-fun long_data_generator(){}
 
 
 //integer data generator function
-// unsigned
+// unsigned. write the code in future
+
+
+
 
 fun main()
 {
-    //variable assignment
+    //variable assignment for testing most used function
     var f_n:Int = 0
     var s_n:Int = 0
     val symbole_user: String ="+"
-    println("kotlin compiler fuzzer(kai) 2026 GSOC")
+    val user_set_limit:Int = 5673
+    var fuzz_test_int = int_data_generator(user_set_limit)
+    var i =0
+
     //generate the integer value for user requirement
     //print(int_data_generator(100))
-    var fuzz_test_int = int_data_generator(578)
-    for(i in fuzz_test_int)
-    {
-        f_n = i
-        println(f_n)
-        s_n = i
-    }
+
 
 
     println("---------------Simple calculator-------------------")
-    //This is user requirement and user use the app
+    //This is user used app process
     /*println("Enter First Number : ")
     val f_n = readln().toInt()
     println("Enter the Symbole :")
     val symbole_user = readLine()
     println("Enter second Number :")
     val s_n = readln().toInt()*/
-    when(symbole_user)
+
+    //fuzzing testing loop
+    while(i <= user_set_limit)
     {
-        "+" -> println(f_n + s_n)
-        "-"-> println(f_n - s_n)
-        "*" -> println(f_n * s_n)
-        "/" -> println(f_n / s_n)
-        else -> println("Unknown symbole :(")
+        f_n = fuzz_test_int[i]
+        s_n = fuzz_test_int[i]
+
+        //Application most used function
+        when (symbole_user)
+        {
+            "+" -> println(f_n + s_n)
+            "-" -> println(f_n - s_n)
+            "*" -> println(f_n * s_n)
+            "/" -> println(f_n / s_n)
+            else -> println("Unknown symbole :(")
+        }
+        i++
     }
 }
